66fb52ef491060c09fa91041ed46c13b70973497
ksenkso <yazunya@gmail.com>
2019-12-09 00:27:19 +0300

    build

---
+++ dist/precache-manifest.1a0ae9e77c567838ef1c7ed41785ff0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2652f51644a85141204c",
+    "url": "/css/app.aad3468d.css"
+  },
+  {
+    "revision": "83f1fdab6fea086f0f9dc4e79b24bab1",
+    "url": "/img/logo.83f1fdab.png"
+  },
+  {
+    "revision": "7fc663e2a279c742ea496443052ad387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d81f87a8acb66be0886",
+    "url": "/js/about.f540bdbd.js"
+  },
+  {
+    "revision": "2652f51644a85141204c",
+    "url": "/js/app.b5bdc278.js"
+  },
+  {
+    "revision": "0fe465340801ea2b0373",
+    "url": "/js/chunk-vendors.5ccb3e99.js"
+  },
+  {
+    "revision": "3d3649300668914e79df4f3b969878ed",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
