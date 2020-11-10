6c02c4e6a17e6824664a953a06b441fdb6733737
ksenkso <yazunya@gmail.com>
2020-11-10 22:22:10 +0300

    refactor(search): adjust style and fix behaviour
    Now the slider is hidden until the object is set;
    Added loader stub and error message

--- src/components/ScpSearch.vue
+++ src/components/ScpSearch.vue
@@ -1,109 +1,149 @@
 <template>
-    <div class="scp-search">
-        <form @submit.prevent="onFormSubmit" class="form">
-            <label for="search">Введите номер объекта:</label>
-            <input type="number" v-model="query" name="search" id="search" ref="search" aria-label="Search">
-            <button>Найти</button>
-        </form>
-        <div class="scp-carousel">
-            <button
-                    @click="switchObject(-1)"
-                    class="scp-carousel__button scp-carousel__button_prev"
-                    aria-label="Предыдущий объект">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.45 45.49"><line y1="22.74" x2="49.67" y2="22.74" style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:7px"/><polygon points="84.22 22.74 49.67 2.79 49.67 42.69 84.22 22.74" style="fill:#fff;stroke:#fff;stroke-miterlimit:10;stroke-width:3.2251867020804847px"/></svg>
-            </button>
-            <ScpCard :object="object"/>
-            <button
-                    @click="switchObject(1)"
-                    class="scp-carousel__button scp-carousel__button_next"
-                    aria-label="Следующий объект">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.45 45.49"><line y1="22.74" x2="49.67" y2="22.74" style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:7px"/><polygon points="84.22 22.74 49.67 2.79 49.67 42.69 84.22 22.74" style="fill:#fff;stroke:#fff;stroke-miterlimit:10;stroke-width:3.2251867020804847px"/></svg>
-            </button>
-        </div>
+  <div class="scp-search">
+    <form @submit.prevent="onFormSubmit" class="form">
+      <label for="search">Введите номер объекта:</label>
+      <input type="number" v-model="query" name="search" id="search" ref="search" aria-label="Search">
+      <button>Найти</button>
+    </form>
+    <div class="scp-carousel" v-if="object">
+      <button
+          @click="switchObject(-1)"
+          class="scp-carousel__button scp-carousel__button_prev"
+          aria-label="Предыдущий объект">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.45 45.49">
+          <line y1="22.74" x2="49.67" y2="22.74" style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:7px"/>
+          <polygon points="84.22 22.74 49.67 2.79 49.67 42.69 84.22 22.74"
+                   style="fill:#fff;stroke:#fff;stroke-miterlimit:10;stroke-width:3.2251867020804847px"/>
+        </svg>
+      </button>
+      <ScpCard :object="object"/>
+      <button
+          @click="switchObject(1)"
+          class="scp-carousel__button scp-carousel__button_next"
+          aria-label="Следующий объект">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.45 45.49">
+          <line y1="22.74" x2="49.67" y2="22.74" style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:7px"/>
+          <polygon points="84.22 22.74 49.67 2.79 49.67 42.69 84.22 22.74"
+                   style="fill:#fff;stroke:#fff;stroke-miterlimit:10;stroke-width:3.2251867020804847px"/>
+        </svg>
+      </button>
     </div>
+    <div class="loader" v-if="isLoading">
+      Loading...
+    </div>
+    <div class="error" v-if="error">
+      {{ error }}
+    </div>
+  </div>
 </template>
 
 <script>
-    import {api} from '../utils';
-    import ScpCard from './ScpCard';
-    export default {
-        name: "ScpSearch",
-        components: {ScpCard},
-        data() {
-            return {
-                query: '',
-                object: null,
-                number: null
-            }
-        },
-        methods: {
-            async onFormSubmit() {
-                const number = parseInt(this.query);
-                if (!isNaN(number)) {
-                    this.number = number;
-                    const response = await api.get('/objects/' + number);
-                    if (response) {
-                        this.object = response.data[0];
-                    }
-                }
-            },
-            async switchObject(count) {
-                const response = await api.get('/objects/' + (this.number + count));
-                if (response) {
-                    this.object = response.data[0];
-                    this.number += count;
-                }
-            }
+import {api} from '../utils';
+import ScpCard from './ScpCard';
+import {Errors} from '@/utils.js';
 
-        }
+export default {
+  name: 'ScpSearch',
+  components: {ScpCard},
+  data() {
+    return {
+      query: '',
+      object: null,
+      isLoading: false,
+      error: null,
+      number: 0,
     }
+  },
+  methods: {
+    async onFormSubmit() {
+      const number = parseInt(this.query);
+      if (!isNaN(number)) {
+        this.getObject(number)
+      }
+    },
+    async switchObject(count) {
+      const nextNumber = this.number + count;
+      if (nextNumber > 0) {
+        this.getObject(nextNumber);
+      }
+    },
+    getObject(number) {
+      this.isLoading = true;
+      api.get('/objects/' + number)
+          .then(response => {
+            this.isLoading = false;
+            this.number = number;
+            this.replaceObjectFromResponse(response);
+          })
+          .catch(() => {
+            this.error = Errors.NOT_FOUND;
+          })
+    },
+    replaceObjectFromResponse(response) {
+      if (response.data && response.data[0]) {
+        this.object = response.data[0];
+        this.error = null;
+      } else {
+        this.error = Errors.NOT_FOUND;
+      }
+    }
+
+  },
+}
 </script>
 
 <style scoped lang="sass">
 .scp-carousel
-    display: flex
-    justify-content: space-between
-    &__button
-        min-width: 32px
-        max-width: 64px
-        width: 100%
-        min-height: 64px
-        background-size: contain
-        &_prev svg
-            transform: rotate(180deg)
+  display: flex
+  justify-content: space-between
+
+  &__button
+    min-width: 32px
+    max-width: 64px
+    width: 100%
+    min-height: 64px
+    background-size: contain
+
+    &_prev svg
+      transform: rotate(180deg)
 
 
 .scp-search
-    padding: 12px
-    display: grid
-    grid-template-rows: 1fr auto
-    grid-template-columns: 1fr
+  padding: 12px
+  display: grid
+  grid-template-rows: 1fr auto
+  grid-template-columns: 1fr
+
 form
-    display: grid
-    font-size: 20px
-    grid-template-rows: repeat(2, 1fr)
-    grid-template-columns: repeat(2, 1fr)
-    grid-template-areas: "label label" "input button"
-    grid-column-gap: .5em
-    > *
-        margin-bottom: 12px
+  display: grid
+  font-size: 20px
+  grid-template-rows: repeat(2, 1fr)
+  grid-template-columns: repeat(2, 1fr)
+  grid-template-areas: "label label" "input button"
+  grid-column-gap: .5em
+
+  > *
+    margin-bottom: 12px
+
 input
-    border-radius: 5px
-    font-size: inherit
-    border: 1px solid #bbb
-    padding: 4px
-    grid-area: input
-    min-width: 100px
+  border-radius: 5px
+  font-size: inherit
+  border: 1px solid #bbb
+  padding: 4px
+  grid-area: input
+  min-width: 100px
+
 button
-    font-size: inherit
-    grid-area: button
-    border-radius: 10px
-    border: 1px solid #bbb
-    background: linear-gradient(to bottom, #966,#633,#300)
-    box-shadow: 0 1px 3px rgba(0,0,0,.5)
-    color: #eeeeee
-    cursor: pointer
+  font-size: inherit
+  grid-area: button
+  border-radius: 10px
+  border: 1px solid #bbb
+  background: linear-gradient(to bottom, #966, #633, #300)
+  box-shadow: 0 1px 3px rgba(0, 0, 0, .5)
+  color: #eeeeee
+  cursor: pointer
+
 label
-    text-align: center
-    grid-area: label
+  text-align: center
+  grid-area: label
 </style>
